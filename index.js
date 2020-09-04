e1bb2d78eb1252dc41e96af2ddb49d3604b6c269
Bowie <bowie@thinkful.com>
2020-09-04 06:30:32 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function dynInput(cbox) {
+    if (cbox.checked) {
+        if (cbox.name === "apple-pay" || cbox.name === "paypal") {
+            return;
+        }
+        const input = document.createElement("input");
+        input.type = "text";
+        input.name = `${cbox.name} ` + "username";
+        input.placeholder = "Username";
+        input.required = true;
+        const div = document.createElement("div");
+        div.id = cbox.name;
+        div.appendChild(input);
+        document.querySelector(`.${cbox.name}`).appendChild(div);
+    } else {
+        document.getElementById(cbox.name).remove();
+    }
+}
